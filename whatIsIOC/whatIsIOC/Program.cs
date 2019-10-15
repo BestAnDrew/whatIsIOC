6e00ec0ace42110eeb4667999a2ca3c1dc95b309
Paul Wang <1043469124@qq.com>
2019-10-15 22:57:59 +0800

    update dapper & design pattern

---
+++ whatIsIOC/whatIsIOC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whatIsIOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //what is Dependency Inversion Principle Explained Practically in C#
+            // Dependence Inversion Principle
+            //依赖倒置原则
+
+
+            //Interface Segregation Principle。
+            //接口隔离原则
+
+            //Single Responsibility Principle
+            //单一职责原则
+
+            //Open Closed Principle
+            //开闭原则
+        }
+    }
+}
